adf62caea2ef298f4ea90bde120e1be3df73a62a
ChoiGiSung <chlrltjd159@naver.com>
2020-07-29 18:03:27 +0900

    로그 찍기 연습

--- spring/gusetbookexam/src/main/java/gusetbookexam/interceptor/LogInterceptor.java
+++ spring/gusetbookexam/src/main/java/gusetbookexam/interceptor/LogInterceptor.java
@@ -1,20 +1,28 @@
 package gusetbookexam.interceptor;
 
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LogInterceptor extends HandlerInterceptorAdapter {
-
+	
+	//log예제
+	private Logger logger=LoggerFactory.getLogger(this.getClass());
+	
+	
 	//인터셉터는 다 작성하고나서 등록해야함 web config에
 	
 	//컨트롤러의 메소드가 실행되고 나서 실행
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		System.out.println(handler.toString()+"가 종료되었습니다."+modelAndView.getViewName()+"을 view로 사용합니다");
+		logger.debug("{} 가 종료 되었스빈다.{} 를 view로 사용합니다",handler.toString(),modelAndView.getViewName());
+		//System.out.println(handler.toString()+"가 종료되었습니다."+modelAndView.getViewName()+"을 view로 사용합니다");
 	}
 
 	
@@ -22,11 +30,15 @@ public class LogInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		System.out.println(handler.toString()+"를 호출 했스빈다.");
+		
+		logger.debug("{} 를 호출했다",handler.toString());
+		
+		//System.out.println(handler.toString()+"를 호출 했스빈다.");
 		return true;
 	}
 	
 	//
+
 
 	
 	
